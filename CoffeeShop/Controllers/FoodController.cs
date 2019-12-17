1c13d4bcc7085d0448846421365298387a1a16b2
Lauren <laurenelizamax@icloud.com>
2019-12-17 09:57:37 -0600

    first commit

---
+++ CoffeeShop/Controllers/FoodController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoffeeShop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FoodController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<FoodController> _logger;
+
+        public FoodController(ILogger<FoodController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "food", "bar", "baz", "from the food controller" };
+        }
+    }
+}
